cf708f0df07f9c33c6e5c7b8b2cca3c17d1c86f9
Sunnie <rainywish0916@gmail.com>
2026-07-09 23:48:08 -0400

    Apply custom cottagecore styling theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,7 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+THEME = 'simple'
+STATIC_PATHS = ['css']
+CUSTOM_CSS = 'css/custom.css'
